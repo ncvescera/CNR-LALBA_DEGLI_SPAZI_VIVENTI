3c8743062548e19a51c2ad264ceec99409aa0e21
Nicolò Vescera <slayercandy@icloud.com>
2016-09-06 11:35:01 +0200

    Modificato algoritmo conversione pdf txt

---
+++ pdf2txt.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+import os
+
+#file must be a string
+def pdf2html(file):
+	print "Converting pdf to html ..."
+	os.system("java -jar PDFToHTML.jar "+file+" out.html")
+	print "Done :D"
+	print "Delating img tags ..."
+	noimgs("out.html")
+	print "Done :D"
+#end function
+
+def noimgs(file):
+	array = list()
+	
+	with open(file,"r") as line:
+		for temp in line:
+			if "img" not in temp and "src" not in temp:
+				array.append(temp)
+				
+	#end with
+	
+	docHtml = open("out.html","w")
+	
+	for row in array:
+		docHtml.write(row)
+	docHtml.close()
+	
+#end function noimgs
+def nospaces(file):
+	f = open(file,"r")
+	lines = f.readlines()
+	f.close()
+
+	f = open(file,"w")
+	for line in lines:
+		f.write(line.replace(" ",""))
+	f.close()
+
+def html2txt(file):
+	print "Parsing html to txt ..."
+
+	os.system("./HtmL " + file)
+	nospaces("testo.txt")
+	os.system("rm " + file)
+	
+	print "Done :D"
+
+
+pdf2html("file.pdf")
+
+html2txt("out.html")
