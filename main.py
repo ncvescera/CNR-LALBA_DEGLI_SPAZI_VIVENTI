f1c15ffc2d2874a01162afd5ba761d3f219abc6f
Nicolò Vescera <slayercandy@icloud.com>
2016-09-09 15:21:52 +0200

    Aggiornato file

--- main.py
+++ main.py
@@ -3,15 +3,31 @@
 import os
 import sys
 import re
+import geonames.adapters.search
 
-from credential import *
+from credential1 import *
 from Country import *
+
+dictionary = ["area","areas","located"]
+
+def ckLib():
+	print "Checking libraries ..."
+	print "Check: poppler-utils ..."
+	os.system("sudo apt-get install poppler-utils -y")
+
+	print "Check: psycopg2 ..."
+	os.system("sudo apt-get install python-psycopg2 -y")
+	
+	print "Check: geonames ..."
+	os.system("sudo add-apt-repository ppa:ubuntugis/ppa -y -qq")
+	os.system("sudo apt-get update -y -qq")
+	
+	print "Done :D"
 
 def pdf2txt(file):
 	#conversione pdf to txt
 	print "Parsing PDF file to txt ..."
-	os.system("sudo apt-get install poppler-utils -y")
-	os.system("pdftotext -l 3 "+file+" out.txt") #converte solo le prime 3 pagine
+	os.system("pdftotext -l 4 "+file+" out.txt") #converte solo le prime 3 pagine
 	print "Done :D"
 
 def getUpperWords(file):
@@ -23,20 +39,24 @@ def getUpperWords(file):
 
 	for row in rows:
 		words = row.split(" ")
+		haveToAdd = False
 		for word in words:
-			if word.istitle():
-				#regular expression serve per cercare e sostituire un determinato set di caratteri
-				regularExpression = re.compile('[^a-zA-Z]')
-				upperWords.append(regularExpression.sub('',word))
+			if word == dictionary[0] or word == dictionary[1] or word == dictionary[2]:
+				haveToAdd = True
+				break
+		if haveToAdd == True:
+			for word in words:
+				if word.istitle():
+					#regular expression serve per cercare e sostituire un determinato set di caratteri
+					regularExpression = re.compile('[^a-zA-Z]')
+					upperWords.append(regularExpression.sub('',word))
 
 	return upperWords #array di stringhe
 
 
 def connectdb():
-
-	os.system("sudo apt-get install python-psycopg2 -y")
 	import psycopg2
-	
+
 	print "Connecting to database ..."
 	conn = psycopg2.connect("dbname="+dbname+" user="+user+" password="+password+" host="+host+" port="+port)
 	if conn:
@@ -70,7 +90,7 @@ def optimazedMatch(ids):
 	for elem in ids:
 		add = False
 		for idN in newIds:
-			if elem.city == idN.city and elem.nation == idN.nation:
+			if elem.city == idN.city:
 				add = False
 				break
 			else:
@@ -81,16 +101,13 @@ def optimazedMatch(ids):
 
 	return newIds
 
-def ckNations(words,matches):
-	ultimateMatches = []
-	for match in matches:
-		nation = match.nation.split("/")
-		for word in words:
-			if nation[0] == word:
-				for elem in words:
-					if nation[1] == elem:
-						ultimateMatches.append(match)
-	return ultimateMatches
+def fetchGeonames(matches):
+	"""
+	os.system("gn_search -p query malaysia -p max_rows 3 dsoprea > geonames.txt")
+	"""
+	pass
+
+ckLib()
 
 pdf2txt(sys.argv[1]) #arg 1 passato allo scritp
 words = getUpperWords("out.txt")
@@ -107,5 +124,4 @@ print "Connection succesful :D"
 matches = matchWords(words,connection)
 for elem in matches:
 	print elem.city + " "+ elem.nation
-
 
